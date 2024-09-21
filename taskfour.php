bf0a0c40eab194062724409e957d56567fd8f1f3
Abhiram8171 <abhiramron7@gmail.com>
2024-09-21 15:48:45 +0530

    Add files via upload

---
+++ taskfour.php
@@ -0,0 +1,6 @@
+<?php
+    $name = "Abhiram G A";
+    date_default_timezone_set("Asia/Kolkata"); 
+    $current_time = date("Y-m-d H:i:s"); 
+    echo("Hello $name, right now the time is $current_time");
+?>
